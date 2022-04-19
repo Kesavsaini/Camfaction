eecb2844584f47ac060a8fc8e2c02a7f1d9a47c3
YASH <yashpokharna2002@gmail.com>
2022-04-19 14:29:28 +0530

    homepage

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -4,7 +4,6 @@ import Navbar from "../../components/Navbar/Navbar";
 import Aboutmebox from "../../components/Aboutmebox/Aboutme";
 import Post from "../../components/Post/Post";
 import NavButton from "../../components/NavButtons/NavButton";
-import Infobox from "../../components/Infobox/Infobox";
 
 const Homepage=()=>{
     return(
@@ -15,26 +14,15 @@ const Homepage=()=>{
      <Aboutmebox/>
      <div className="midpage">
      <NavButton/>
-<<<<<<< HEAD
-<<<<<<< HEAD
-     <Post/>
-     </div>
-     <Infobox/> 
-=======
-=======
->>>>>>> 64c5f521253e839012b4cc7fa99ba6868487d8c1
      <Post/> 
      <Post/> 
      <Post/> 
      <Post/> 
      <Post/> 
      <Post/> 
-<<<<<<< HEAD
->>>>>>> 44547146112370f98921064d2c7629a3d474a06d
-=======
->>>>>>> 64c5f521253e839012b4cc7fa99ba6868487d8c1
      </div>
      
+     </div>
      </div>
      </>
     );
